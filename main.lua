47bdac8b2fb3a0d10a212fa111e03e19832d82e8
Rodion Kokotov <rodionkokotov@gmail.com>
2024-04-10 17:23:23 +0700

    add sprint

--- main.lua
+++ main.lua
@@ -3,8 +3,11 @@ function love.load()
     player.x = 200
     player.y = 300
     player.speed = 5
+    player.sprint = 10
+    player.startspeed = player.speed
 end
 function love.update(dt)
+
     if love.keyboard.isDown("w") then --movement up
         player.y = player.y - player.speed
     end
@@ -17,10 +20,17 @@ function love.update(dt)
     if love.keyboard.isDown("d") then
         player.x = player.x + player.speed -- movement right
     end
+    if love.keyboard.isDown("lshift") then
+        player.speed = player.sprint
+    else
+        player.speed = player.startspeed
+    end
+
 end
 function love.draw()
     if love.keyboard.isDown("space") then
         love.graphics.print("hello world!", 300,400)
     end
-    love.graphics.circle("line",player.x,player.y,100)
+    --love.graphics.print(player.speed,200,300)
+    love.graphics.circle("line",player.x,player.y,30)
 end
